330d871d27ebdbbf2bc3bcc3990cc5a726297627
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-05-19 11:51:22 +0530

    suffix trie

--- suffix_trie.js
+++ suffix_trie.js
@@ -1,44 +1,44 @@
-// class TrieNode{
-//     constructor(){
-//         this.children ={}
-//         this.isEnd =false
-//     }
-// }
+class TrieNode{
+    constructor(){
+        this.children ={}
+        this.isEnd =false
+    }
+}
 
-// class SuffixTrie{
-//     constructor() {
-//         this.root = new TrieNode(null)
-//     }
-//     insert(word){
-//         for(let i=0;i<word.length;i++){
-//             this.suffix(i,word)
-//         }
-//     }
-//     suffix(index,word){
-//         let curr = this.root;
-//         for(let i=index;i<word.length;i++){
-//             let char =word[i]
-//             if(!curr.children[char]){
-//                 curr.children[char]=new TrieNode()
-//             }
-//             curr = curr.children[char]
-//         }
-//         curr.isEnd = true
-//         // console.log(this.root.children['p']);
-//     }
-//     search(word){
-//         let curr = this.root
-//         for(let i=0;i<word.length;i++){
-//             let char = word[i]
-//             if(!(char in curr.children)){
-//                 return false
-//             }
-//             curr = curr.children[char]
-//         }
-//         return true
-//     }
-// }
-// const suffix = new SuffixTrie()
-// suffix.insert('prayag')
-// suffix.insert('prajwal')
-// console.log(suffix.search('prayag'));
+class SuffixTrie{
+    constructor() {
+        this.root = new TrieNode(null)
+    }
+    insert(word){
+        for(let i=0;i<word.length;i++){
+            this.suffix(i,word)
+        }
+    }
+    suffix(index,word){
+        let curr = this.root;
+        for(let i=index;i<word.length;i++){
+            let char =word[i]
+            if(!curr.children[char]){
+                curr.children[char]=new TrieNode()
+            }
+            curr = curr.children[char]
+        }
+        curr.isEnd = true
+        // console.log(this.root.children['p']);
+    }
+    search(word){
+        let curr = this.root
+        for(let i=0;i<word.length;i++){
+            let char = word[i]
+            if(!(char in curr.children)){
+                return false
+            }
+            curr = curr.children[char]
+        }
+        return true
+    }
+}
+const suffix = new SuffixTrie()
+suffix.insert('prayag')
+suffix.insert('prajwal')
+console.log(suffix.search('prayag'));
